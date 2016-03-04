3c2dcb34506eb361b8d8ba59198a4c18cafab810
Issac Trotts <issac.trotts@gmail.com>
2016-03-04 10:57:46 -0800

    Add profile script.

---
+++ profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+N=$1
+if [ -z "$N" ]; then
+  echo "Usage $0 N"
+  echo " where N is the input to generator."
+  exit 1
+fi
+
+ghc -prof -fprof-auto -rtsopts getstats.hs 
+time (./generator $N | ./getstats +RTS -p > /dev/null)
+
